07e3fe714ee8346b99c0325763582bf5b05bf94b
Davide Giuseppe Farella <37957092+4face-studi0@users.noreply.github.com>
2018-08-22 04:20:35 +0200

    CircleCi setup

--- enviromentSetup.sh
+++ enviromentSetup.sh
@@ -1,18 +1,16 @@
-function copyEnvVarsToLocalProperties {
-    LOCAL_PROPERTIES=$HOME"/.gradle/local.properties"
-    export LOCAL_PROPERTIES
-    echo "Local Properties should exist at $LOCAL_PROPERTIES"
- 
-    if [ ! -f "$LOCAL_PROPERTIES" ]; then
-        echo "Local Properties does not exist"
- 
-        echo "Creating Local Properties file..."
-        touch $LOCAL_PROPERTIES
- 
-        echo "Writing bintray.user to local.properties..."
-        echo "bintray.user=$bintrayUser" >> $LOCAL_PROPERTIES
+LOCAL_PROPERTIES=$HOME"/.gradle/local.properties"
+export LOCAL_PROPERTIES
+echo "Local Properties should exist at $LOCAL_PROPERTIES"
 
-        echo "Writing bintray.apikey to local.properties..."
-        echo "bintray.apikey=$bintrayApikey" >> $LOCAL_PROPERTIES
-    fi
-}
+if [ ! -f "$LOCAL_PROPERTIES" ]; then
+    echo "Local Properties does not exist"
+
+    echo "Creating Local Properties file..."
+    touch $LOCAL_PROPERTIES
+
+    echo "Writing bintray.user to local.properties..."
+    echo "bintray.user=$bintrayUser" >> $LOCAL_PROPERTIES
+
+    echo "Writing bintray.apikey to local.properties..."
+    echo "bintray.apikey=$bintrayApikey" >> $LOCAL_PROPERTIES
+fi
