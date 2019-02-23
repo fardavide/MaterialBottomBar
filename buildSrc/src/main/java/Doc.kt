55aa0c0573a668ffa38152fc60d866109994dede
4face-studi0 <37957092+4face-studi0@users.noreply.github.com>
2019-02-23 16:27:39 +0700

    Better Dokka configuration with Docs

--- buildSrc/src/main/java/Doc.kt
+++ buildSrc/src/main/java/Doc.kt
@@ -1,11 +1,14 @@
 import org.gradle.kotlin.dsl.KotlinBuildScript
 import org.gradle.kotlin.dsl.apply
-import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.withType
 import org.jetbrains.dokka.gradle.DokkaAndroidTask
 
+@Suppress("unused")
 fun KotlinBuildScript.applyDokka() {
+    apply( plugin = "com.android.library" )
+    apply( plugin = "kotlin-android" )
     apply( plugin = "org.jetbrains.dokka-android" )
-    configure<DokkaAndroidTask> {
+    tasks.withType( DokkaAndroidTask::class ) {
         outputFormat = "html"
         outputDirectory = "docs"
     }
